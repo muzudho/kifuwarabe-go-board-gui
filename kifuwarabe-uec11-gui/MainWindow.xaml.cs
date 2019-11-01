fd51e18d63de621693556ce410d0519a18acbf89
むずでょ <muzudho1@gmail.com>
2019-11-01 23:03:45 +0900

    盤の符号☆（＾～＾）

--- kifuwarabe-uec11-gui/MainWindow.xaml.cs
+++ kifuwarabe-uec11-gui/MainWindow.xaml.cs
@@ -14,11 +14,25 @@
     public partial class MainWindow : Window
     {
         private List<Ellipse> Stones { get; set; }
+        private List<Label> RowLabels { get; set; }
+        private List<Label> ColumnLabels { get; set; }
         private Random Random { get; set; }
+
+        /// <summary>
+        /// 19本の線を引くから20分割だが、符号の列を1つ足すぜ☆（＾～＾）
+        /// </summary>
+        private static int BoardDiv = 21;
+
+        /// <summary>
+        /// 符号の1列☆（＾～＾）
+        /// </summary>
+        private static int SignLen = 1;
 
         public MainWindow()
         {
             this.Stones = new List<Ellipse>();
+            this.RowLabels = new List<Label>();
+            this.ColumnLabels = new List<Label>();
 
             // 乱数のタネは固定しておいた方がデバッグしやすいぜ☆（＾～＾）
             this.Random = new Random(0);
@@ -49,8 +63,8 @@
             board.Height = shortenEdge;
             var paddingLeft = board.Width * 0.05;
             var paddingTop = board.Height * 0.05;
-            var columnInterval = board.Width / 20;
-            var rowInterval = board.Height / 20;
+            var columnInterval = board.Width / BoardDiv;
+            var rowInterval = board.Height / BoardDiv;
 
             // タテ線20本☆（＾～＾）
             var verticalLines = new List<Line>() {
@@ -110,10 +124,10 @@
                 verticalLine.StrokeThickness = 1.5;
                 Panel.SetZIndex(verticalLine, 110);
                 // 盤の幅を20で割ろうぜ☆（＾～＾）
-                verticalLine.X1 = boardLeft + board.Width * 0.05 + board.Width / 20 * column;
+                verticalLine.X1 = boardLeft + board.Width * 0.05 + columnInterval * (column + SignLen);
                 verticalLine.Y1 = boardTop + board.Height * 0.05;
                 verticalLine.X2 = verticalLine.X1;
-                verticalLine.Y2 = verticalLine.Y1 + board.Height * 0.905; // 線の太さのせいでうまく合わん
+                verticalLine.Y2 = verticalLine.Y1 + rowInterval * 18;
             }
             // ヨコ線をタテに並べるぜ☆（＾～＾）
             for (var row = 0; row < 19; row++)
@@ -127,9 +141,9 @@
                 horizontalLine.StrokeThickness = 1.5;
                 Panel.SetZIndex(horizontalLine, 110);
                 // 盤の幅を20で割ろうぜ☆（＾～＾）
-                horizontalLine.X1 = boardLeft + board.Width * 0.05;
-                horizontalLine.Y1 = boardTop + board.Height * 0.05 + board.Height / 20 * row;
-                horizontalLine.X2 = horizontalLine.X1 + board.Width * 0.905; // 線の太さのせいでうまく合わん
+                horizontalLine.X1 = boardLeft + board.Width * 0.05 + columnInterval * SignLen;
+                horizontalLine.Y1 = boardTop + board.Height * 0.05 + rowInterval * row;
+                horizontalLine.X2 = horizontalLine.X1 + columnInterval * 18;
                 horizontalLine.Y2 = horizontalLine.Y1;
             }
             // Trace.WriteLine($"verticalLine0 ({verticalLine0.X1}, {verticalLine0.Y1})  ({verticalLine0.X2}, {verticalLine0.Y2})");
@@ -139,14 +153,40 @@
             {
                 var stone = this.Stones[i];
                 var row = i / 19;
-                var column = i % 19;
+                var column = i % 19 + SignLen;
 
-                stone.Width = board.Width / 20 * 0.8;
-                stone.Height = board.Height / 20 * 0.8;
+                stone.Width = board.Width / BoardDiv * 0.8;
+                stone.Height = board.Height / BoardDiv * 0.8;
                 stone.StrokeThickness = 1.5;
                 // 盤の幅を21で割ろうぜ☆（＾～＾）
                 Canvas.SetLeft(stone, boardLeft + paddingLeft - stone.Width / 2 + columnInterval * column);
                 Canvas.SetTop(stone, boardTop + paddingTop - stone.Height / 2 + rowInterval * row);
+            }
+
+            // 列の符号を描こうぜ☆（＾～＾）？
+            for (var column = 0; column < 19; column++)
+            {
+                var label = this.ColumnLabels[column];
+
+                label.FontSize = columnInterval * 0.9;
+                label.Width = columnInterval * 1.8;
+                label.Height = rowInterval * 1.8;
+                // 文字位置の調整は　良い方法がないので勘で調整☆（＾～＾）
+                Canvas.SetLeft(label, boardLeft + paddingLeft * 1.05 - label.Width / 3 + columnInterval * (column + SignLen));
+                Canvas.SetTop(label, boardTop + paddingTop - label.Height / 2 + rowInterval * 19);
+            }
+
+            // 行の符号を描こうぜ☆（＾～＾）？
+            for (var row = 0; row < 19; row++)
+            {
+                var label = this.RowLabels[row];
+
+                label.FontSize = columnInterval * 0.9;
+                label.Width = columnInterval * 1.8;
+                label.Height = rowInterval * 1.8;
+                // 盤の幅を21で割ろうぜ☆（＾～＾）
+                Canvas.SetLeft(label, boardLeft + paddingLeft - label.Width / 2 + columnInterval * 0);
+                Canvas.SetTop(label, boardTop + paddingTop - label.Height / 2 + rowInterval * row);
             }
         }
 
@@ -172,7 +212,6 @@
 
                 var stone = new Ellipse();
                 stone.Name = $"stone{i}";
-                this.Stones.Add(stone);
                 stone.Width = 10;
                 stone.Height = 10;
                 stone.StrokeThickness = 1.5;
@@ -191,7 +230,39 @@
                 // 盤☆（＾～＾）
                 Canvas.SetLeft(stone, 0);
                 Canvas.SetTop(stone, 0);
+                this.Stones.Add(stone);
                 canvas.Children.Add(stone);
+            }
+
+            // 列の符号を描こうぜ☆（＾～＾）？
+            for (var column = 0; column < 19; column++)
+            {
+                var label = new Label();
+                label.Name = $"columnLabel{column + 1}";
+                Panel.SetZIndex(label, 130);
+                label.Content = (char)(65 + (column<8?column:column+1)); // 65はAsciiCodeのA。コンピューター囲碁では I は抜く慣習。
+                this.ColumnLabels.Add(label);
+                canvas.Children.Add(label);
+            }
+
+            // 行の番号を描こうぜ☆（＾～＾）？
+            for (var row = 0; row < 19; row++)
+            {
+                var number = 19 - row;
+                var label = new Label();
+                label.Name = $"rowLabel{number}";
+                Panel.SetZIndex(label, 130);
+                if (9<number)
+                {
+                    label.Content = number;
+                }
+                else
+                {
+                    // 文字位置の調整がうまく行かないので勘で調整☆（＾～＾）
+                    label.Content = $"  {number}";
+                }
+                this.RowLabels.Add(label);
+                canvas.Children.Add(label);
             }
         }
     }
